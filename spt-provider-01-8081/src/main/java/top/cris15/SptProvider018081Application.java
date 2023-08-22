de78c995b0545f9f3b3624c8b52323c617dd9801
Cris-lxd <woaini1120620371@gmail.com>
2023-08-22 20:49:31 +0800

    first commit

---
+++ spt-provider-01-8081/src/main/java/top/cris15/SptProvider018081Application.java
@@ -0,0 +1,13 @@
+package top.cris15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptProvider018081Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SptProvider018081Application.class, args);
+    }
+
+}
---
+++ spt-provider-01-8081/src/main/java/top/cris15/bean/Depart.java
@@ -0,0 +1,13 @@
+package top.cris15.bean;
+
+import lombok.Data;
+
+/**
+ * date： 2023/8/22
+ * time： 20:46
+ * author： cris
+ * description：
+ **/
+@Data
+public class Depart {
+}
