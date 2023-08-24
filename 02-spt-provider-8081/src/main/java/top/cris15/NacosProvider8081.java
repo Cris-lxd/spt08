7a0278d2308c082b714736ce3701f18d4f837bfc
Cris <woaini1120620371@gmail.com>
2023-08-24 15:40:52 +0800

    增加nacos注册中心
    1.增加nacos provider
    2.修改字符unicode中文乱码问题
    3.修改springoot版本  3.1.2 -> 3.0.6

---
+++ 02-spt-provider-8081/src/main/java/top/cris15/NacosProvider8081.java
@@ -0,0 +1,13 @@
+package top.cris15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosProvider8081 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProvider8081.class, args);
+    }
+
+}
