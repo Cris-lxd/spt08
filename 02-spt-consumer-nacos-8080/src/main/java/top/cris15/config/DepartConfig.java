f07a0c886b249cffeab5f5e5f04de4f6648c2d4b
Cris <woaini1120620371@gmail.com>
2023-08-28 11:26:20 +0800

    增加nacos-消费者

---
+++ 02-spt-consumer-nacos-8080/src/main/java/top/cris15/config/DepartConfig.java
@@ -0,0 +1,17 @@
+package top.cris15.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class DepartConfig {
+
+
+    @LoadBalanced       // 负载均衡方式调用
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ 02-spt-consumer-nacos-8080/src/main/java/top/cris15/controller/DepartController.java
@@ -0,0 +1,54 @@
+package top.cris15.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import top.cris15.bean.Depart;
+import top.cris15.utils.R;
+
+@RestController
+@RequestMapping("consumer/depart")
+public class DepartController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    // 直连方式
+    //public static final String SERVICE_PROVIDER = "http://localhost:8081/provider/depart";
+
+    // 微服务方式
+    public static final String SERVICE_PROVIDER = "http://depart-provider/provider/depart";
+
+    @PostMapping("/save")
+    public R saveHandle(@RequestBody Depart depart){
+        String url = SERVICE_PROVIDER +"/save";
+        return restTemplate.postForObject(url,depart,R.class);
+    }
+
+    @DeleteMapping("/del/{id}")
+    public void deleteHandle(@PathVariable int id){
+        String url = SERVICE_PROVIDER + "/del/" + id;
+        restTemplate.delete(url);
+    }
+
+    @PutMapping("/update")
+    public void updateHandle(@RequestBody Depart depart){
+        String url = SERVICE_PROVIDER + "/update";
+        restTemplate.put(url,depart);
+    }
+
+    @GetMapping("/get/{id}")
+    public R getHandle(@PathVariable int id){
+        String url = SERVICE_PROVIDER + "/get/" + id;
+        return restTemplate.getForObject(url,R.class);
+    }
+
+    @GetMapping("/list")
+    public R listHandle(){
+        String url = SERVICE_PROVIDER + "/list";
+        R r = restTemplate.getForObject(url,R.class);
+        return r;
+    }
+
+}
